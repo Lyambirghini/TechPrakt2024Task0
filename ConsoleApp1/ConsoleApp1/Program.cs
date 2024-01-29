2a8e1c4d6890518b0fcc5a6c9b3864a632be3fbb
Максим Сівченко <ipz224_smt@student.ztu.edu.ua>
2024-01-29 20:52:32 +0200

    Create gitignore and Console progect

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿double number1, number2;
+
+Console.WriteLine("Введіть перше число:");
+while (!double.TryParse(Console.ReadLine(), out number1))
+{
+    Console.WriteLine("Некоректний ввід. Будь ласка, введіть число знову:");
+}
+
+Console.WriteLine("Введіть друге число:");
+while (!double.TryParse(Console.ReadLine(), out number2))
+{
+    Console.WriteLine("Некоректний ввід. Будь ласка, введіть число знову:");
+}
+
+double sum = number1 + number2;
+
+Console.WriteLine($"Сума чисел {number1} і {number2} дорівнює: {sum}");
+
+Console.ReadLine();
